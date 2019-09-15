c6c6c194519b6e6660d58eac46220bc0458f0939
Robert Lee <bobbydwise1@hotmail.com>
2019-09-15 13:41:11 -0700

    Initial commit.

---
+++ scripts.js
@@ -0,0 +1,52 @@
+/*
+This problem was asked by Amazon.
+
+Given a N by M matrix of numbers, print out the matrix in a clockwise spiral.
+
+For example, given the following matrix:
+
+[[1,  2,  3,  4,  5],
+ [6,  7,  8,  9,  10],
+ [11, 12, 13, 14, 15],
+ [16, 17, 18, 19, 20]]
+
+You should print out the following:
+
+1
+2
+3
+4
+5
+10
+15
+20
+19
+18
+17
+16
+11
+6
+7
+8
+9
+14
+13
+12
+
+*/
+
+
+
+$(document).ready(function() {
+
+  $('#form1').submit(function(){
+    event.preventDefault()
+    input1 = $('#input-1').val()
+    input1 = parseInt(input1)
+    input2 = $('#input-2').val()
+    input2 = parseInt(input2)
+    console.log(input1, input2)
+    $('#output-1').text(input1,input2)
+  })
+
+});
