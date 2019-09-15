1d5d9b02368b6e68908bc6a033fa67b1254eb91f
Robert Lee <bobbydwise1@hotmail.com>
2019-09-15 13:46:05 -0700

    Add vars.

--- scripts.js
+++ scripts.js
@@ -32,10 +32,20 @@ You should print out the following:
 14
 13
 12
-
 */
 
+cosnt spiral = (yourArrays) => {
+  
+}
 
+
+const test1 = 
+[[1,2,3,4,5],
+[6,7,8,9,10],
+[11,12,13,14,15],
+[16,17,18,19,20],
+[21,22,23,24,25]
+]]
 
 $(document).ready(function() {
 
